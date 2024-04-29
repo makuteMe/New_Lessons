abc60ff609552fcb2a0151ea5bced2beb42c067a
makuteMe <167203235+makuteMe@users.noreply.github.com>
2024-04-29 18:30:23 +0300

    Add files via upload

---
+++ homework_7.py
@@ -0,0 +1,29 @@
+x = 38
+print('Hello')
+if x < 0:
+    print('Less then 0')
+print('Goodbye')
+
+a, b = 10, 5
+if a > b:
+    print('a > b')
+if a > b and a > 0:
+    print('success')
+if (a > b) and (a > 0 or b < 1000):
+    print('success')
+if 5 < b and b < 10:
+    print('success')
+if '34' > '123':
+    print('success')
+if '123' > '12':
+    print('success')
+if [1, 2] > [1, 1]:
+    print('success')
+if '6' > 5:
+    print('success')
+if [5, 6] > 5:
+    print('success')
+if '6' != 5:
+    print('success')
+
+
