41d5f5d15a9793ed84494ccfba914a31dddb7348
makuteMe <167203235+makuteMe@users.noreply.github.com>
2024-05-14 20:19:24 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,9 @@
+my_string = input('Введите какой-то текст ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(f'Это первый символ строки: {my_string[0]}')
+print(f'Это первый символ строки: {my_string[-1]}')
+
+
