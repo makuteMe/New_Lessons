c479670893fd6de892b3f3f2c8ff92bbcb4994ae
makuteMe <167203235+makuteMe@users.noreply.github.com>
2024-05-15 18:39:37 +0300

    Add files via upload

---
+++ task_4.py
@@ -0,0 +1,4 @@
+my_list = ['string', 1, [2, 4, 'hello'], 'apple', 56, 'banana', 'juice']
+print(my_list, '\n', my_list[0], my_list[-1], '\n' , my_list[2:6], sep='')
+my_list[2] = 'changed'
+print(my_list)
